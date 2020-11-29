66a2960b33215315ba4b0ffe7da5c5f65cc07def
Tyler Pena <tylerpena@me.com>
2020-11-29 00:58:23 -0800

    Factory contract, router/pair layouts

--- src/OpdexV1Core/OpdexV1Pair.cs
+++ src/OpdexV1Core/OpdexV1Pair.cs
@@ -2,7 +2,80 @@ using Stratis.SmartContracts;
 
 public class OpdexV1Pair : SmartContract
 {
-    public OpdexV1Pair(ISmartContractState smartContractState) : base(smartContractState)
+    public OpdexV1Pair(ISmartContractState smartContractState, Address tokenA, Address tokenB) : base(smartContractState)
     {
+        Factory = Message.Sender;
+        TokenA = tokenA;
+        TokenB = tokenB;
+    }
+
+    public Address Factory
+    {
+        get => PersistentState.GetAddress(nameof(Factory));
+        private set => PersistentState.SetAddress(nameof(Factory), value);
+    }
+    
+    public Address TokenA
+    {
+        get => PersistentState.GetAddress(nameof(TokenA));
+        private set => PersistentState.SetAddress(nameof(TokenA), value);
+    }
+    
+    public Address TokenB
+    {
+        get => PersistentState.GetAddress(nameof(TokenB));
+        private set => PersistentState.SetAddress(nameof(TokenB), value);
+    }
+    
+    public ulong ReserveA
+    {
+        get => PersistentState.GetUInt32(nameof(ReserveA));
+        private set => PersistentState.SetUInt64(nameof(ReserveA), value);
+    }
+    
+    public ulong ReserveB
+    {
+        get => PersistentState.GetUInt64(nameof(ReserveB));
+        private set => PersistentState.SetUInt64(nameof(ReserveB), value);
+    }
+
+    public struct MintEvent
+    {
+        [Index] public Address Sender;
+        public ulong AmountA;
+        public ulong AmountB;
+    }
+    
+    public struct BurnEvent
+    {
+        [Index] public Address Sender;
+        public ulong AmountA;
+        public ulong AmountB;
+        [Index] public ulong To;
+    }
+
+    public struct SwapEvent
+    {
+        [Index] public Address Sender;
+        public ulong AmountAIn;
+        public ulong AmountBIn;
+        public ulong AmountAOut;
+        public ulong AmountBOut;
+        [Index] public Address To;
+    }
+
+    public void Mint()
+    {
+        
+    }
+
+    public void Burn()
+    {
+        
+    }
+
+    public void Swap()
+    {
+        
     }
 }
--- src/OpdexV1Core/OpdexV1Router.cs
+++ src/OpdexV1Core/OpdexV1Router.cs
@@ -2,7 +2,83 @@
 
 public class OpdexV1Router : SmartContract
 {
-    public OpdexV1Router(ISmartContractState smartContractState) : base(smartContractState)
+    public OpdexV1Router(ISmartContractState smartContractState, Address factory, Address wcrs) : base(smartContractState)
     {
+        Factory = factory;
+        WCRS = wcrs;
     }
+
+    public override void Receive()
+    {
+        Assert(Message.Sender == WCRS, "OpdexV2: UNACCEPTED_CRS");
+        base.Receive();
+    }
+
+    public Address Factory
+    {
+        get => PersistentState.GetAddress(nameof(Factory));
+        set => PersistentState.SetAddress(nameof(Factory), value);
+    }
+    
+    public Address WCRS
+    {
+        get => PersistentState.GetAddress(nameof(WCRS));
+        set => PersistentState.SetAddress(nameof(WCRS), value);
+    }
+
+    # region Liquidity
+    public void AddLiquidity()
+    {
+        
+    }
+
+    public void AddLiquidityCRS()
+    {
+        
+    }
+
+    public void RemoveLiquidity()
+    {
+        
+    }
+
+    public void RemoveLiquidityCRS()
+    {
+        
+    }
+    #endregion
+    
+    #region Swaps
+
+    public void SwapExactTokensForTokens()
+    {
+        
+    }
+
+    public void SwapTokensForExactTokens()
+    {
+        
+    }
+
+    public void SwapExactCRSForTokens()
+    {
+        
+    }
+
+    public void SwapTokensForExactCRS()
+    {
+        
+    }
+
+    public void SwapExactTokensForCRS()
+    {
+        
+    }
+
+    public void swapCRSForExactTokens()
+    {
+        
+    }
+    
+    #endregion
 }
